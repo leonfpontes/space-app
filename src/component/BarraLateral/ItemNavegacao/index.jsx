70e9d8910b4ed377a80ed271923f5bca3b443c6a
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-24 16:32:52 -0300

    criacao de estilos de lista lateral

---
+++ src/component/BarraLateral/ItemNavegacao/index.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+const ItemListaEstilizado = styled.li`
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: ${(props) => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+    display: flex;
+    align-items: center;
+    gap: 22px;
+`
+
+
+const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo=false}) => {
+    return(
+        <ItemListaEstilizado $ativo={ativo}>
+            <img src={ativo ? iconeAtivo: iconeInativo} alt="Ícone do item da lista" />
+            {children}
+        </ItemListaEstilizado>
+    )
+}
+
+export default ItemNavegacao  

ab9603af0a1109bfd59839b98a342060cc79229a
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-24 14:04:24 -0300

    assets public, header e reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import styled from "styled-components";
+import EstilosGlobais from "./component/GlobalStyles";
+import Cabecalho from "./component/Cabecalho";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+    </FundoGradiente>
   )
 }
 
